48ee5c94d7f22d0fae902325aaac72734abb9353
r0nn13 <delyon.ronnie@gmail.com>
2015-01-15 00:10:54 +0300

    bugy skeleton for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bugyroku.wsgi --log-file -
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nemesis/Work/Eclipse/workspace/bugyroku/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
